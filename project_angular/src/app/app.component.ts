701ab2bb9d388194cf8db9cdf81be42fa398372a
MedisaSatara <medisa.satara@edu.fit.ba>
2022-05-24 01:32:42 +0200

    done

---
+++ project_angular/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, forwardRef, ViewChild } from '@angular/core';
+import { SharedService } from './shared.service';
+import { EventsComponent } from './events/events.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project_angular';
+  @ViewChild(forwardRef(() => EventsComponent))
+    eventsComponent: EventsComponent;
+    
+  constructor(public shared:SharedService){}
+
+  sportNameSelected(event:any)
+  {
+    this.eventsComponent.getSportEvents(event);
+  }
+
+  categoryNameSelected(event:any)
+  {
+    this.eventsComponent.getCategoryEvents(event);
+  }
+  competitionNameSelected(event:any)
+  {
+    this.eventsComponent.getCompetitionEvents(event);
+  }
+  ShowAllEvents= function()
+  {
+  }
+}
---
+++ project_angular/src/app/events/events.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+
+
+export class EventsComponent implements OnInit {
+
+  constructor(public service:SharedService) { }
+  EventList: any=[];
+
+  ngOnInit(): void {
+    this.refreshEventList();
+  }
+
+  refreshEventList()
+  {
+    this.service.getAllEvent().subscribe(data=>{
+      this.EventList=data as any [];
+    })
+  }
+
+  getSportEvents(event:any)
+  {
+    this.service.getEventsBySport(event).subscribe(data=>this.onSuccess(data))
+  }
+  getCategoryEvents(event:any)
+  {
+    this.service.getEventsByCategory(event).subscribe(data=>this.onSuccess(data))
+  }
+  getCompetitionEvents(event:any)
+  {
+    this.service.getEventsByCompetition(event).subscribe(data=>this.onSuccess(data))
+  }
+  onSuccess(data:any[])
+  {
+    this.EventList=data;
+  }
+
+
+ 
+  
+
+  
+ 
+
+}
